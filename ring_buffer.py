98ca13835edd7a672b4adeac22c105c54470ae06
Mikhail <mmv047@gmail.com>
2019-04-17 15:50:40 +0300

    add preprocess module and ring buffer

---
+++ ring_buffer.py
@@ -0,0 +1,29 @@
+class RingBuf:
+    def __init__(self, size):
+        self.data = [None] * (size + 1)
+        self.start = 0
+        self.end = 0
+
+    def append(self, element):
+        self.data[self.end] = element
+        self.end = (self.end + 1) % len(self.data)
+
+        if self.end == self.start:
+            self.start = (self.start + 1) % len(self.data)
+
+    # TODO: implement random sample batch
+    def sample_batch(self, num):
+        pass
+
+    def __getitem__(self, idx):
+        return self.data[(self.start + idx) % len(self.data)]
+
+    def __len__(self):
+        if self.end < self.start:
+            return self.end + len(self.data) - self.start
+        else:
+            return self.end - self.start
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
