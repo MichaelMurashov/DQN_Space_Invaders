f9336e475c2a8e2598a29d07a047254704db9708
Mikhail <mmv047@gmail.com>
2019-08-22 10:42:48 +0300

    update ring buffer

--- ring_buffer.py
+++ ring_buffer.py
@@ -1,34 +1,63 @@
 import random
+import numpy as np
 
 
-class RingBuf:
+class ReplayBuffer(object):
     def __init__(self, size):
-        self.data = [None] * (size + 1)
-        self.start = 0
-        self.end = 0
-
-    def append(self, element):
-        self.data[self.end] = element
-        self.end = (self.end + 1) % len(self.data)
-
-        if self.end == self.start:
-            self.start = (self.start + 1) % len(self.data)
-
-    def sample_batch(self, num):
-        batch = []
-        for i in random.sample(list(range(self.__len__())), k=num):
-            batch.append(self.__getitem__(i))
-        return batch
-
-    def __getitem__(self, idx):
-        return self.data[(self.start + idx) % len(self.data)]
+        """Create Replay buffer.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        """
+        self._storage = []
+        self._maxsize = size
+        self._next_idx = 0
 
     def __len__(self):
-        if self.end < self.start:
-            return self.end + len(self.data) - self.start
-        else:
-            return self.end - self.start
+        return len(self._storage)
 
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
+    def append(self, curr_state, action, reward, next_state, is_terminal):
+        data = (curr_state, action, reward, next_state, is_terminal)
+
+        if self._next_idx >= len(self._storage):
+            self._storage.append(data)
+        else:
+            self._storage[self._next_idx] = data
+        self._next_idx = (self._next_idx + 1) % self._maxsize
+
+    def _encode_sample(self, idxes):
+        curr_states, actions, rewards, next_states, is_terminals = [], [], [], [], []
+        for i in idxes:
+            data = self._storage[i]
+            obs_t, action, reward, obs_tp1, done = data
+            curr_states.append(np.array(obs_t, copy=False))
+            actions.append(np.array(action, copy=False))
+            rewards.append(reward)
+            next_states.append(np.array(obs_tp1, copy=False))
+            is_terminals.append(done)
+        return np.array(curr_states), np.array(actions), np.array(rewards), np.array(next_states), np.array(is_terminals)
+
+    def sample_batch(self, batch_size):
+        """Sample a batch of experiences.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            batch of observations
+        act_batch: np.array
+            batch of actions executed given obs_batch
+        rew_batch: np.array
+            rewards received as results of executing act_batch
+        next_obs_batch: np.array
+            next set of observations seen after executing act_batch
+        done_mask: np.array
+            done_mask[i] = 1 if executing act_batch[i] resulted in
+            the end of an episode and 0 otherwise.
+        """
+        idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
+        return self._encode_sample(idxes)
